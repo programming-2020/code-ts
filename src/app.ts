2649ecd0cbad431a97cb4f3f364ed98a51aefbb4
githubdmn <dmninbox@gmail.com>
2021-07-25 16:10:14 +0200

    First commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+console.log("My ts app new");
+console.log("Another 1");
+
+import { run } from "./test/hello";
+
+
+run();
+
+
+
+
---
+++ src/test/hello.ts
@@ -0,0 +1,5 @@
+
+
+export function run() {
+  console.log("Hello - run()");
+}
